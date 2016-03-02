462f2cbdb72c080ba93517223671cb1433ee4d83
Blake Ziolkowski <zylviij@gmail.com>
2016-03-02 00:58:56 -0600

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install Caskroom/cask/github-desktop
+brew install Caskroom/cask/sublime-text
+brew install Caskroom/cask/firefox
+brew install Caskroom/cask/google-chrome
+brew install Caskroom/cask/pycharm
+brew install Caskroom/cask/java
+brew install python3
+brew tap caskroom/cask
